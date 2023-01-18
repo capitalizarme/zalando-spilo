a13c2e2eeac571663720aba00abb79fc7cdea957
Roy Alvear <racl@gulix.cl>
2023-01-18 12:10:32 -0300

    check requirements and optimize

--- postgres-appliance/build_scripts/install_pgh3.sh
+++ postgres-appliance/build_scripts/install_pgh3.sh
@@ -4,30 +4,36 @@
 # This extension integrates with the PostGIS geometry type. 
 # Namely Point, Polygon and MultiPolygon geometries are supported.
 #
+# Require:
+#     cmake \
+#     make \
+#     gcc \
+#     libtool \
+#     clang-format \
+#     cmake-curses-gui \
+#     lcov \
+#     doxygen \
+#     postgresql-server-dev-all
+#
+# Author: Roy Alvear <racl@fedoraproject.org>
 #
 set -ex
 
 version=$1
 PGVERSION=$2
 
-Plib='/usr/lib/postgresql'
-Pshare='/usr/share/postgresql'
+if [ ! -d "pgh3" ]; then 
+    git clone https://github.com/dlr-eoc/pgh3.git
+fi
 
-apt update
-apt install -y \
-    cmake \
-    make \
-    gcc \
-    libtool \
-    clang-format \
-    cmake-curses-gui \
-    lcov \
-    doxygen \
-    postgresql-server-dev-all
+# pgh3 require h3
+if [ ! -d "h3" ]; then
+    git clone https://github.com/uber/h3.git
+fi
 
-git clone https://github.com/dlr-eoc/pgh3.git
-git clone https://github.com/uber/h3.git
+cp -ar pgh3 pgh3_${version}
 
+# build h3
 mkdir h3/build
 cd h3/build
 git checkout v3.7.2
@@ -35,16 +41,15 @@ cmake -DCMAKE_C_FLAGS=-fPIC ..
 make
 make install
 
-cd ../../pgh3
+# build pgh3
+cd ../../pgh3_${version}
 make
 make install
 cd ..
 
-if [ ${PGVERSION} -ne ${version} ]; then
-    cp -ar ${Plib}/${PGVERSION}/lib/pgh3.so ${Plib}/${version}/lib/pgh3.so
-    cp -ar ${Pshare}/${PGVERSION}/extension/pgh3.control ${Pshare}/${version}/extension/pgh3.control
-    cp -ar ${Pshare}/${PGVERSION}/extension/pgh3--0.3.0.sql ${Pshare}/${version}/extension/pgh3--0.3.0.sql
-fi
-
 # clean source
-rm -rf h3 pgh3
+rm -rf h3/build pgh3_${version}
+
+if [ ${PGVERSION} -eq ${version} ]; then
+    rm -rf h3 pgh3
+fi
